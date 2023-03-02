bb4a3d8ab40d84973dc783974af74b5b02a535c2
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-03-02 11:58:41 +0100

    Implemented the player - display and movement ok!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,30 @@
-console.log('Hello world!');
+class Player {
+
+    constructor() {
+        this.positionX = 0;
+        this.positionY = 0;
+        this.playerElm = document.getElementById('player');
+    }
+
+    moveLeft() {
+        this.positionX--;
+        this.playerElm.style.left = this.positionX + 'vw';
+    }
+
+    moveRight() {
+        this.positionX++;
+        this.playerElm.style.left = this.positionX + 'vw';
+    }
+};
+
+const myPlayer = new Player();
+
+// Attach Event Listeners
+
+document.addEventListener('keydown', event => {
+    if(event.key === 'ArrowLeft' || event.key === 'a') {
+        myPlayer.moveLeft();
+    } else if(event.key === 'ArrowRight' || event.key === 'd') {
+        myPlayer.moveRight();
+    }
+});
